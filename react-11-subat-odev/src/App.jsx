88d93ed615c7de6387202f54a79f53f6e1531522
Mahmut Paşa <mahcoder01@proton.me>
2026-02-11 21:04:56 +0300

    temel ekleme 1

--- react-11-subat-odev/src/App.jsx
+++ react-11-subat-odev/src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
+//Maksimum insan, minumum ai.
+//  - Gemini (Pro)
+// Bu sefer zorlandığım kısım dışında sizin önceden yazdığınız kodları temel
+// ederek yazdım.
 function App() {
   return (
     <>
+      <title>React 11 Subat Odevleri</title>
+      <div>
+        <label>E-Posta: </label>
+        <input type="email" placeholder='E-Posta giriniz' />
+      </div>
+      <div>
+        <label>Parola: </label>
+        <input type="password" placeholder='Parola giriniz' />
+      </div>
+      <button>Giriş Yap</button>
+      <button>Temizle</button>
     </>
   )
 }
