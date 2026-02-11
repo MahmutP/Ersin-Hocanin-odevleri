c7c5edd0cd08a461e0cdc87372946d548986e41f
Mahmut Paşa <mahcoder01@proton.me>
2026-02-11 21:13:30 +0300

    Sonunda bitti

--- react-11-subat-odev/src/App.jsx
+++ react-11-subat-odev/src/App.jsx
@@ -6,19 +6,54 @@ import './App.css'
 // Bu sefer zorlandığım kısım dışında sizin önceden yazdığınız kodları temel
 // ederek yazdım.
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+    setError('');
+  }
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+    setError('');
+  }
+  const clickReset = () => {
+    setEmail('');
+    setPassword('');
+    setError('');
+  }
+  const clickSubmit = () => {
+    if (email === '' || password === '') {
+      setError('Lütfen e-posta ve parola giriniz');
+    } else if (email === 'ornek@xyz.com' && password === '12345') {
+      setLoginSuccess(true);
+      setError('');
+    } else {
+      setError('E-posta veya parola hatalı');
+    }
+  }
+
+  if (loginSuccess) {
+    return (
+      <div>Giriş Başarılı</div>
+    )
+  }
+
   return (
     <>
       <title>React 11 Subat Odevleri</title>
       <div>
         <label>E-Posta: </label>
-        <input type="email" placeholder='E-Posta giriniz' />
+        <input type="email" placeholder='E-Posta giriniz' onChange={handleChangeEmail} value={email} />
       </div>
       <div>
         <label>Parola: </label>
-        <input type="password" placeholder='Parola giriniz' />
+        <input type="password" placeholder='Parola giriniz' onChange={handleChangePassword} value={password} />
       </div>
-      <button>Giriş Yap</button>
-      <button>Temizle</button>
+      <div>{error}</div>
+      <button onClick={clickSubmit}>Giriş Yap</button>
+      <button onClick={clickReset}>Temizle</button>
     </>
   )
 }
